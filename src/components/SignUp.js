522761cae192a75b02da66e8b1a2df922817bf93
TohidEq <TohidEq@gmail.com>
2023-02-15 16:38:10 +0330

    SignUp section

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,64 +1,108 @@
-import styles from '../styles/components/SignUp.module.css';
+import styles from "../styles/components/SignUp.module.css";
 
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import Input from './Input';
+import { useSignUpEmailPassword } from "@nhost/react";
+
+import { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import Input from "./Input";
+
+import Spinner from "./Spinner";
 
 const SignUp = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const {
+    signUpEmailPassword,
+    isLoading,
+    isSuccess,
+    isError,
+    needsEmailVerification,
+    error,
+  } = useSignUpEmailPassword();
 
   const handleOnSubmit = async (e) => {
     e.preventDefault();
+
+    signUpEmailPassword(email, password, {
+      displayName: "$(firstName) $(lastName)".trim(),
+      metadata: {
+        firstName,
+        lastName,
+      },
+    });
   };
+
+  if (isSuccess) {
+    return <Navigate to={"/"} replace={true} />;
+  }
+
+  const disableForm = isLoading || needsEmailVerification;
 
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <div className={styles['logo-wrapper']}>
-          <img src={process.env.PUBLIC_URL + 'logo.svg'} alt="logo" />
+        <div className={styles["logo-wrapper"]}>
+          <img src={process.env.PUBLIC_URL + "logo.svg"} alt="logo" />
         </div>
-
-        <form onSubmit={handleOnSubmit} className={styles.form}>
-          <div className={styles['input-group']}>
-            <Input
-              label="First name"
-              value={firstName}
-              onChange={e => setFirstName(e.target.value)}
-              required
-            />
-            <Input
-              label="Last name"
-              value={lastName}
-              onChange={e => setLastName(e.target.value)}
-              required
-            />
+        {needsEmailVerification ? (
+          <div className={styles["logo-wrapper"]}>
+            Please check your mailbox and follow the verification link to verify
+            your account.
           </div>
-          <Input
-            type="email"
-            label="Email address"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            required
-          />
-          <Input
-            type="password"
-            label="Create password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            required
-          />
+        ) : (
+          <form onSubmit={handleOnSubmit} className={styles.form}>
+            <div className={styles["input-group"]}>
+              <Input
+                disabled={disableForm}
+                label="First name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+              <Input
+                disabled={disableForm}
+                label="Last name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+            </div>
+            <Input
+              disabled={disableForm}
+              type="email"
+              label="Email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <Input
+              disabled={disableForm}
+              type="password"
+              label="Create password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
 
-          <button type="submit" className={styles.button}>
-            Create account
-          </button>
-        </form>
+            <button
+              type="submit"
+              disabled={disableForm}
+              className={styles.button}
+            >
+              {isLoading ? <Spinner size="sm" /> : "Create account"}
+            </button>
+            {isError ? (
+              <p className={styles["error-text"]}>{error?.message}</p>
+            ) : null}
+          </form>
+        )}
       </div>
 
       <p className={styles.text}>
-        Already have an account?{' '}
+        Already have an account?{" "}
         <Link to="/sign-in" className={styles.link}>
           Sign in
         </Link>
